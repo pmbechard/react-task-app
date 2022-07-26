87575303956a1b2ec2bbd3cb8e3a4ed5ebed2f7e
Peyton Bechard <pbechard@my.yorku.ca>
2022-07-26 20:58:02 +0800

    Adds Overview component to handle listing tasks and form input in App component to add new tasks

--- src/App.js
+++ src/App.js
@@ -1,7 +1,57 @@
 import './App.css';
+import React, { Component } from 'react';
+import uniqid from 'uniqid';
+import Overview from './components/Overview';
 
-function App() {
-  return <div className='App'></div>;
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      task: { text: '', id: uniqid() },
+      tasks: [],
+    };
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      task: {
+        text: e.target.value,
+        id: this.state.task.id,
+      },
+    });
+  };
+
+  onSubmitTask = (e) => {
+    e.preventDefault();
+    this.setState({
+      tasks: this.state.tasks.concat(this.state.task),
+      task: { text: '', id: uniqid() },
+    });
+  };
+
+  render() {
+    const { task, tasks } = this.state;
+    return (
+      <div>
+        <div>
+          <form onSubmit={this.onSubmitTask}>
+            <label htmlFor='taskInput'>Enter task</label>
+            <input
+              type='text'
+              id='taskInput'
+              value={task.text}
+              onChange={this.handleChange}
+            />
+            <button type='submit'>Add task</button>
+          </form>
+        </div>
+        <div>
+          <Overview tasks={tasks} />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Overview.js
@@ -0,0 +1,13 @@
+const Overview = (props) => {
+  const { tasks } = props;
+
+  return (
+    <ul>
+      {tasks.map((task) => {
+        return <li key={task.id}>{task.text}</li>;
+      })}
+    </ul>
+  );
+};
+
+export default Overview;
